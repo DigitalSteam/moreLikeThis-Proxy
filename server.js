df6a90a349f091feff22b01cd2e5df48e0dd751a
Nils Gudbranson <nils.gudbran@gmail.com>
2018-08-04 16:11:41 -0700

    deployment with ec2 and docker

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const parser = require('body-parser');
+const request = require('request');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('./public'));
+app.use(allowCrossDomain);
+app.use(parser.json());
+app.use(parser.urlencoded({
+  extended: true,
+}));
+
+app.get('/api/games/:1/more-games', (req, res) => {
+  request.get('http://ec2-54-215-240-119.us-west-1.compute.amazonaws.com/api/games/:1/more-games', (err, response, body) => {
+    console.log(body);
+    res.send(body);
+  });
+});
 
 app.listen(port, () => {
-  console.log(`server running at: http://localhost:${port}`);
+  console.log(`server running at port: ${port}`);
 });
